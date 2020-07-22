d97cae1c932f1937dab8cb82648549bb89548a0d
himalhack <51692864+himalhack@users.noreply.github.com>
2020-07-22 21:42:49 +0545

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import phonenumbers
+from phonenumbers import geocoder
+from phonenumbers import carrier
+
+print("\n" + "  Phone information Gathering Tool   "+"\n")
+
+num=input('Enter Phone Number with Country Code: ')
+full_num = '+' + str(num)
+phone_number = phonenumbers.parse(full_num)
+print("The phone number is from "+geocoder.description_for_number(phone_number,'en') + " .")
+print("The Telecom provider of " + str(num) + " is "+ carrier.name_for_number(phone_number, 'en'))
