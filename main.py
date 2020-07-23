1ef57418fb7c9c3ffb97b0357b9b3467d82625e3
himalhack <51692864+himalhack@users.noreply.github.com>
2020-07-23 12:50:14 +0545

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from phonenumbers import carrier
 
 print("\n" + "  Phone information Gathering Tool   "+"\n")
 
-num=input('Enter Phone Number with Country Code: ')
+num = input('Enter Phone Number with Country Code: ')
 full_num = '+' + str(num)
 phone_number = phonenumbers.parse(full_num)
-print("The phone number is from "+geocoder.description_for_number(phone_number,'en') + " .")
-print("The Telecom provider of " + str(num) + " is "+ carrier.name_for_number(phone_number, 'en'))
+print("The phone number is from " +
+      geocoder.description_for_number(phone_number, 'en') + " .")
+print("The Telecom provider of " + str(num) + " is " + carrier.name_for_number(phone_number, 'en'))
